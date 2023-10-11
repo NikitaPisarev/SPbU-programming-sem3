5dda2025184858f113a1cdfdf95ec2ae1b349d46
Nikita Pisarev <nikgooder@gmail.com>
2023-10-11 19:07:09 +0300

    feat: add a simple thread pool

---
+++ HWs/HW3/MyThreadPool/MyThreadPool.cs
@@ -0,0 +1,143 @@
+namespace MyThreadPool;
+
+using System.Collections.Concurrent;
+using System.Threading;
+
+public class MyThreadPool
+{
+    private readonly Thread[] _threads;
+    private readonly ConcurrentQueue<Action> _taskQueue;
+    private readonly CancellationTokenSource _cts;
+    private readonly object _lockObject;
+
+
+    public MyThreadPool(int threadNumber)
+    {
+        _threads = new Thread[threadNumber];
+        _taskQueue = new();
+        _cts = new();
+        _lockObject = new();
+
+        for (int i = 0; i < threadNumber; i++)
+        {
+            _threads[i] = new Thread(Work);
+            _threads[i].Start();
+        }
+    }
+
+    public IMyTask<TResult> Submit<TResult>(Func<TResult> function)
+    {
+        if (_cts.Token.IsCancellationRequested)
+        {
+            throw new InvalidOperationException("Thread pool was shut down.");
+        }
+
+        lock (_lockObject)
+        {
+            if (_cts.Token.IsCancellationRequested)
+            {
+                throw new InvalidOperationException("Thread pool was shut down.");
+            }
+
+            var myTask = new MyTask<TResult>(function, this);
+            _taskQueue.Enqueue(myTask.Execute);
+            Monitor.Pulse(_lockObject);
+
+            return myTask;
+        }
+    }
+
+    private void Work()
+    {
+        while (!_cts.Token.IsCancellationRequested)
+        {
+            Action? task = null;
+            lock (_lockObject)
+            {
+                while (!_taskQueue.TryDequeue(out task) && !_cts.Token.IsCancellationRequested)
+                {
+                    Monitor.Wait(_lockObject);
+                }
+            }
+            task?.Invoke();
+        }
+    }
+
+    public void Shutdown()
+    {
+        lock (_lockObject)
+        {
+            _cts.Cancel();
+            Monitor.PulseAll(_lockObject);
+        }
+
+        foreach (var thread in _threads)
+        {
+            thread.Join();
+        }
+    }
+
+    private class MyTask<TResult> : IMyTask<TResult>
+    {
+        private Func<TResult> _function;
+        private MyThreadPool _threadPool;
+        private TResult? _result;
+        private volatile bool _isCompleted = false;
+        private Exception? _exception;
+        private object _syncRoot = new();
+
+        public MyTask(Func<TResult> function, MyThreadPool threadPool)
+        {
+            _function = function;
+            _threadPool = threadPool;
+        }
+
+        public bool IsCompleted => _isCompleted;
+
+        public TResult? Result
+        {
+            get
+            {
+                lock (_syncRoot)
+                {
+                    while (!_isCompleted)
+                    {
+                        Monitor.Wait(_syncRoot);
+                    }
+
+                    if (_exception != null)
+                    {
+                        throw new AggregateException(_exception);
+                    }
+
+                    return _result;
+                }
+            }
+        }
+
+        internal void Execute()
+        {
+            try
+            {
+                _result = _function();
+            }
+            catch (Exception e)
+            {
+                _exception = e;
+            }
+            finally
+            {
+                lock (_syncRoot)
+                {
+                    _isCompleted = true;
+                    Monitor.Pulse(_syncRoot);
+                }
+            }
+        }
+
+        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult?, TNewResult> continuationFunction)
+        {
+            return _threadPool.Submit(() => continuationFunction(Result));
+        }
+    }
+}
