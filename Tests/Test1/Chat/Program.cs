95f5af099d7949109f7b6a48a7142aaf4e7f38f5
Nikita Pisarev <nikgooder@gmail.com>
2023-11-02 17:00:19 +0300

    add test1

---
+++ Tests/Test1/Chat/Program.cs
@@ -0,0 +1,84 @@
+﻿using Chat;
+
+using System.Net;
+using System.Net.Sockets;
+
+static void PrintHelp()
+{
+    Console.WriteLine("""
+        This programme is designed to run a chat which,
+        depending on the command line options, runs either as a client or as a server. 
+
+        Usage:
+        If only the port is specified, the application starts as a server and listens on the port:
+        -------------------------------------------
+        dotnet run <port>
+        -------------------------------------------
+
+        If an IP address and port are specified,
+        the application starts as a client and connects to the specified server:
+        -------------------------------------------
+        dotnet run <ip> <port>
+        -------------------------------------------
+
+        When someone types "exit" into the console, the connection is closed and the applications are terminated.
+        """);
+}
+
+int port = 0;
+
+switch (args.Length)
+{
+    case 1:
+        if (args[0] == "help")
+        {
+            PrintHelp();
+            break;
+        }
+
+        if (!int.TryParse(args[0], out port) || port < 0 || port > 65535)
+        {
+            Console.WriteLine("Incorrect port value.");
+            break;
+        }
+
+        var server = new Server(port);
+        try
+        {
+            await server.StartAsync();
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Client connection error.");
+        }
+        break;
+
+    case 2:
+        if (IPAddress.TryParse(args[0], out _))
+        {
+            Console.WriteLine("Incorrect IP-adress.");
+            break;
+        }
+
+        if (!int.TryParse(args[1], out port) || port < 0 || port > 65535)
+        {
+            Console.WriteLine("Incorrect port value.");
+            break;
+        }
+
+        var client = new Client(args[0], port);
+
+        try
+        {
+            await client.StartAsync();
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Connection to server error.");
+        }
+        break;
+
+    default:
+        PrintHelp();
+        break;
+}
---
+++ Tests/Test1/Chat/Server.cs
@@ -0,0 +1,64 @@
+namespace Chat;
+
+using System.Net;
+using System.Net.Sockets;
+
+/// <summary>
+/// Represents a server that can chat with the client.
+/// </summary
+public class Server
+{
+    private readonly int _port;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Server"/> class.
+    /// </summary>
+    /// <param name="port">The port to listen on.</param>
+    public Server(int port)
+    {
+        _port = port;
+    }
+
+    /// <summary>
+    /// Listen on the port, wait for connection to the client to be established.
+    /// </summary>
+    public async Task StartAsync()
+    {
+        var listener = new TcpListener(IPAddress.Any, _port);
+        listener.Start();
+
+        Console.WriteLine($"Server started on port {_port}.");
+        var client = await listener.AcceptTcpClientAsync();
+        Console.WriteLine("Client connected.");
+
+        using var stream = client.GetStream();
+        using var reader = new StreamReader(stream);
+        using var writer = new StreamWriter(stream) { AutoFlush = true };
+
+        await Task.WhenAny(
+            ReadAsync(reader),
+            WriteAsync(writer));
+
+        client.Close();
+        listener.Stop();
+    }
+
+    private static async Task ReadAsync(StreamReader reader)
+    {
+        string? line;
+        while ((line = await reader.ReadLineAsync()) != "exit")
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    private static async Task WriteAsync(StreamWriter writer)
+    {
+        string? line;
+        while ((line = Console.ReadLine()) != "exit")
+        {
+            await writer.WriteLineAsync("Server: " + line);
+        }
+        await writer.WriteLineAsync(line);
+    }
+}
